5f4f8490a1ce1f6a67adf6dc2f7b69554e4455ec
digitaldeshpande <digitaldeshpande@gmail.com>
2018-05-08 21:11:32 +0530

    Adding all files

---
+++ build_site.R
@@ -0,0 +1,9 @@
+#Set our working directory. 
+#This helps avoid confusion if our working directory is 
+#not our site because of other projects we were 
+#working on at the time. 
+setwd("/Users/Sandeep/Documents/DigitalDeshpande/Website/website")
+
+#render your sweet site. 
+install.packages("rmarkdown")
+rmarkdown::render_site()
